7b390c6d847b28491e629ac1daca2a300d6ee481
Mfuon Leonard <mfolee@gmail.com>
2024-07-10 16:27:27 +0300

    include avenir fonts for the website

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,14 @@
 <script setup lang="ts">
 import LandingPage from '@/components/LandingPage.vue'
+
 </script>
 
 <template>
   <LandingPage/>
 </template>
+
+<style>
+#app {
+  font-family: 'Avenir', sans-serif;
+}
+</style>
